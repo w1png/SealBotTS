86623417d75660ebcf352e92a13d1540967cd199
w1png <wonepng@gmail.com>
2022-03-17 16:07:22 +0300

    Add guild-bridge embeds from mc chat

--- src/MinecraftManager.ts
+++ src/MinecraftManager.ts
@@ -1,9 +1,12 @@
 import * as minecraftProtocol from "minecraft-protocol"
 import * as commandModules from "./minecraftCommands";
 import { ConfigManager as ConfMan } from "./ConfigManager";
+import { client as discordClient } from "./DiscordManager";
+import{ ColorResolvable, MessageEmbed, TextChannel } from "discord.js";
+import * as utils from "./utils";
 
 const commands = Object(commandModules);
-const ConfigManager = new ConfMan("config.json");
+const ConfigManager = new ConfMan("config.json"); 
 
 var client = minecraftProtocol.createClient({
     host: ConfigManager.config["minecraft-server"],
@@ -13,28 +16,54 @@ var client = minecraftProtocol.createClient({
     version: "1.8.9"
 });
 
-function removeColors(text: string) {
+function removeColors(text: string): string {
     return text.replace(/\u00A7[0-9A-FK-OR]/ig, "");
 }
 
-function removeRanks(text: string) {
+function removeRanks(text: string): string {
    return text.replace(/ *\[[^\]]*]/g, '')};
 
 client.on('chat', function(packet: any) {
-    // Listen for chat messages and echo them back.
-
     var msg = JSON.parse(packet.message);
     var username: string;
     var text: string;
+    var color: ColorResolvable;
+
     try {
+        // leave/join messages
         if (msg.text == "Guild > ") {
-            console.log(`${msg.extra[0].text} ${msg.extra[1].text}`); 
+            username = msg.extra[0].text;
+            text = msg.extra[1].text;
+            color = (text.endsWith("joined.")) ? "GREEN": "RED";
+            utils.sendEmbedToChannel(
+                ConfigManager.config["discord-bridge-channel"],
+                new MessageEmbed()
+                    .setAuthor(`${username}${text}`, "https://www.mc-heads.net/avatar/" + username)
+                    .setTimestamp()
+                    .setColor(color)
+            );
         } else if (msg.text == "") {
+            // strip colors and spaces from username and text
             username = removeColors(msg.extra[0].text).replace(/ /g,'');
-            text = removeColors(msg.extra[1].text); 
+            text = removeColors(msg.extra[1].text);
+            // if guild chat
             if (username.startsWith("Guild>")) {
-                username = removeRanks(username.slice(6).slice(0, -1)); 
-                console.log(`${username}: ${text}`);
+                // remove ranks from username to get plain username
+                username = removeRanks(username.slice(6).slice(0, -1));
+                // dont parse own messages
+                if (username == ConfigManager.config["minecraft-username"]) return;
+                if (text.startsWith("!")) {
+                    // parse commands here
+                }
+                // send to bridge chat if not a command
+                utils.sendEmbedToChannel(
+                    ConfigManager.config["discord-bridge-channel"],
+                    new MessageEmbed()
+                        .setAuthor(username, "https://www.mc-heads.net/avatar/" + username)
+                        .setDescription(text)
+                        .setColor("#ADD8E6")
+                        .setTimestamp()
+                );
             }  
         }
     } catch{}
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
 import { ConfigManager as ConfMan } from "./ConfigManager";
+import { client as discordClient } from "./DiscordManager";
+import { TextChannel, MessageEmbed } from "discord.js";
+
+export function sendTextToChannel(channelId: string, text: string): void {
+    (discordClient.channels.cache.get(channelId) as TextChannel).send(text);
+}
+
+export function sendEmbedToChannel(channelId: string, embed: MessageEmbed): void {
+    (discordClient.channels.cache.get(channelId) as TextChannel).send({ embeds: [embed] });
+}
 
 
