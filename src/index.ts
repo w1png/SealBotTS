ab57caff849d0d2f27af47e3ab72f74e2c7688f1
w1png <wonepng@gmail.com>
2022-03-20 21:52:17 +0300

    Fix ConfigManager initialising after imports
    This breaks stuff on first time launch because config.json does not
    exist at this point

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
+export const CONFIG_PATH = "config.json";
+const ConfigManager = new ConfMan(CONFIG_PATH);
+
 import {ConfigManager as ConfMan} from "./ConfigManager";
 import "./DiscordManager";
 import "./MinecraftManager";
-
-export const CONFIG_PATH = "config.json";
-
-const ConfigManager = new ConfMan(CONFIG_PATH);
-
-
-
