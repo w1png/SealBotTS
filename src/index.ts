2e9c61534d9d3420445466718c6b864353d7bc19
w1png <wonepng@gmail.com>
2022-03-20 22:28:33 +0300

    Fix last commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import {ConfigManager as ConfMan} from "./ConfigManager";
+
 export const CONFIG_PATH = "config.json";
 const ConfigManager = new ConfMan(CONFIG_PATH);
 
-import {ConfigManager as ConfMan} from "./ConfigManager";
 import "./DiscordManager";
 import "./MinecraftManager";
