2a057ec2a7fbf425a4c9e5b071cf2b42eb442845
w1png <wonepng@gmail.com>
2022-04-05 13:03:39 +0300

    Fix UserManager

--- src/UserManager.ts
+++ src/UserManager.ts
@@ -1,28 +1,79 @@
 import { db } from "./index";
-import { getDatabaseQueryResult } from "./utils";
+import { ConsoleLogger as ConsLog } from "./ConsoleLogger"; 
+
+const ConsoleLogger = new ConsLog();
+
+export interface User {
+  username: string,
+  discord_id: string
+}
 
 export class UserManager {
-  getUsernameByDiscordId(discord_id: string) { 
+  getUserByDiscordId(discord_id: string): Promise<User> { 
     return new Promise((resolve, reject) => {
       db.get(`SELECT * FROM users WHERE discord_id="${discord_id}"`,(err, row) => {
-         resolve(row.username); 
+        try {
+          let user: User = {
+            username: row.username,
+            discord_id: row.discord_id
+          } 
+          resolve(user);
+        } catch {
+          ConsoleLogger.logWarn(`User <@&${discord_id}> was not found in the database.`);
+          reject(undefined);
+        } 
       });
     });
   }
   
-  getDiscordIdByMinecraftUsername(minecraft_username: string) {
+  getUserByMinecraftUsername(minecraft_username: string): Promise<User> {
     return new Promise((resolve, reject) => {
       db.get(`SELECT * FROM users WHERE username="${minecraft_username}"`,(err, row) => {
-         resolve(row.discord_id);
+        try {
+          let user: User = {
+            username: row.username,
+            discord_id: row.discord_id
+          } 
+          resolve(user);
+        } catch {
+          ConsoleLogger.logWarn(`User ${minecraft_username} was not found in the database.`);
+          reject(undefined);
+        }
       });
     });
   }
 
-  createUser(discord_id: string, username: string) {
-    db.run("INSERT INTO users VALUES (?, ?)", [discord_id, username]); 
+  createUser(user: User): void {
+    ConsoleLogger.log(`<@&${user.discord_id}> was added to the database!`);
+    db.run("INSERT INTO users VALUES (?, ?)", [user.discord_id, user.username]); 
   }
 
-  removeUser(discord_id: string) {
+  removeUser(discord_id: string): void {
+    ConsoleLogger.log(`<@&${discord_id}> was removed from the database!`);
     db.run("DELETE FROM users WHERE discord_id=?", [discord_id]);
+  }
+
+  getUsers(): Promise<Array<User>> {
+    return new Promise((resolve, reject) => {
+      db.all(`SELECT * FROM users`,(err, row) => {
+        try {
+          let userList: Array<User> = [];
+          let user: User;
+
+          for (let userRaw of row) {
+            user = {
+              username: userRaw.username,
+              discord_id: userRaw.discord_id
+            } 
+            userList.push(user);
+          }
+
+          resolve(userList);
+        } catch {
+          ConsoleLogger.logWarn("Failed to get users.");
+          reject([]);
+        }
+      });
+    });
   }
 }
