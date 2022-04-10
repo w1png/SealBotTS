05f469639a9d6a52aa2e0c16b0c0baa70f200f76
w1png <wonepng@gmail.com>
2022-04-10 06:51:18 +0300

    Add error handling

--- src/discordCommands/unverifiedlist.ts
+++ src/discordCommands/unverifiedlist.ts
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { CommandInteraction, MessageEmbed, GuildMember } from "discord.js";
-import { getLoadingEmbed, getGuildMemberUsernameList, doesMemberHaveRole, getNoPermissionEmbed } from "../utils";
+import { getLoadingEmbed, getGuildMemberUsernameList, doesMemberHaveRole, getNoPermissionEmbed, getUnexpectedErrorEmbed } from "../utils";
 import { getUserByMinecraftUsername } from "../UserManager";
 import { ConfigManager as ConfMan } from "../ConfigManager";
 
@@ -22,15 +22,23 @@ export async function execute(interaction: CommandInteraction) {
   });
 
   getGuildMemberUsernameList().then(async (guildMemberList) => {
+    if(!guildMemberList) return interaction.reply({
+      embeds: [new MessageEmbed().setTitle("Everyone is verified!").setDescription("Great job! Everyone is already verified!")]
+    });
+
     let desc = "";
 
     for (let member of guildMemberList) 
       if (!(await getUserByMinecraftUsername(member))) 
         desc += member + "\n";
-   
+    
     return interaction.editReply({
       embeds: [new MessageEmbed().setTitle("Unverified users:").setDescription(desc)],
     });
+  }).catch(() => {
+    interaction.editReply({
+      embeds: [getUnexpectedErrorEmbed()]
+    })
   });
 }
 
